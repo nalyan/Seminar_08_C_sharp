0639c494e361b69d7cf8b4526185339815f51bf3
ilnar <ilnar-sov@mail.ru>
2022-09-05 22:17:09 +0300

    Практические задания к семинару 8

--- Practical tasks/task_01/Program.cs
+++ Practical tasks/task_01/Program.cs
@@ -23,24 +23,30 @@ void PrintMatrix(int[,] matrix)
 }
 int[,] MultiMatrix(int[,] matrix1, int[,] matrix2)
 {
-    int[,] newMatrix = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+    int[,] newMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
     for(int i=0; i<matrix1.GetLength(0); i++)
     {
-        for(int j=0; j<matrix1.GetLength(1); j++)
+        for(int j=0; j<matrix2.GetLength(1); j++)
         {
-            newMatrix[i,j] = matrix1[i,j] * matrix2[i,j];
+            int sum = 0;
+            for(int k=0; k<matrix1.GetLength(1); k++)
+            {
+                sum = sum + matrix1[i,k] * matrix2[k,j];
+            }
+            newMatrix[i,j] = sum;
         }
     }
     return newMatrix;
 }
+
 Console.Clear();
-int[,] matrix1 = CreateMatrix(4,4,0,10);
-int[,] matrix2 = CreateMatrix(4,4,10,20);
+int[,] matrix1 = CreateMatrix(2,3,0,5);
+int[,] matrix2 = CreateMatrix(3,4,0,5);
 PrintMatrix(matrix1);
 PrintMatrix(matrix2);
-if(matrix1.GetLength(0)==matrix2.GetLongLength(0) && matrix1.GetLength(1)==matrix2.GetLongLength(1))
+if(matrix1.GetLength(1)==matrix2.GetLength(0))
 {
     int[,] newMatrix = MultiMatrix(matrix1, matrix2);
     PrintMatrix(newMatrix);
 }
-else Console.WriteLine("Размеры массивов не совпадают");
+else Console.WriteLine("Матрицы нельзя перемножить");
