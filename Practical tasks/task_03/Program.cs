d4f6aa3256cf059aac66e6c645bd8129f2682d5b
nalyan <ilnar-sov@mail.ru>
2022-09-08 21:09:08 +0300

    Практические задания к семинару 8

---
+++ Practical tasks/task_03/Program.cs
@@ -0,0 +1,15 @@
+﻿// 3. Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
+
+Console.Clear();
+int[,,] matrix = {{{10,11,12},{13,14,15}}, {{16,17,18},{19,20,21}}};
+for(int i=0; i<matrix.GetLength(0); i++)
+{
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+        for(int k=0; k<matrix.GetLength(2); k++)
+        {
+            Console.Write($"{matrix[i,j,k]} ({i},{j},{k})  ");
+        }
+        Console.WriteLine();
+    }
+}
